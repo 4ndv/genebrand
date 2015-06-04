bce37f6415f6352e7ae5a1e4bdaec80152a8972f
Andrey Viktorov <andreyviktorov@outlook.com>
2015-06-04 13:54:37 +0700

    Making codeclimate 4.0

--- lib/genebrand/posparser.rb
+++ lib/genebrand/posparser.rb
@@ -3,38 +3,41 @@ module Genebrand
     require 'json'
     require 'fileutils'
 
+    def initialize
+      @parsed = Hash.new
+      @table = Hash.new
+      # Сущ
+      @table["N"] = @parsed["noun"] = Array.new
+      # Мн. число
+      @table["P"] = @parsed["plur"] = Array.new
+      # Глаг. прич, пер, непер
+      @table["V"] = @parsed["verb_part"] = Array.new
+      @table["t"] = @parsed["verb_trans"] = Array.new
+      @table["i"] = @parsed["verb_intrans"] = Array.new
+      # Прилаг
+      @table["A"] = @parsed["adj"] = Array.new
+    end
+
     def parse(filename)
       if not File.exists?(filename)
         raise "File not found: #{filename}"
         return
       end
 
-      parsed = Hash.new
-      table = Hash.new
-      # Сущ
-      table["N"] = parsed["noun"] = Array.new
-      # Мн. число
-      table["P"] = parsed["plur"] = Array.new
-      # Глаг. прич, пер, непер
-      table["V"] = parsed["verb_part"] = Array.new
-      table["t"] = parsed["verb_trans"] = Array.new
-      table["i"] = parsed["verb_intrans"] = Array.new
-      # Прилаг
-      table["A"] = parsed["adj"] = Array.new
 
       File.open(filename, 'r').each_line do |line|
         data = line.split("\t")
 
         if !is_numeric?(data[0]) and (/\A[a-zA-Z0-9]{2,10}\z/.match(data[0]) != nil)
           data[1].split("").each do |partofsp|
-            if table.has_key?(partofsp)
-              table[partofsp].push(data[0])
+            if @table.has_key?(partofsp)
+              @table[partofsp].push(data[0])
             end
           end
         end
       end
 
-      return parsed
+      return @parsed
     end
 
     def parseandsave(filename)
